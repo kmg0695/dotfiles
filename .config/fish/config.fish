5968a33e7ac837d6ccf11bfad36264c23fa1b2bf
kmg0695 <79101566+kmg0695@users.noreply.github.com>
2024-10-10 02:16:27 -0400

    config.fish cleanup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,12 +5,12 @@ end
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
-    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" hook $argv | source
 else
     if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
         . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
     else
-        set -x PATH "/opt/homebrew/Caskroom/miniconda/base/bin" $PATH
+        set -x PATH /opt/homebrew/Caskroom/miniconda/base/bin $PATH
     end
 end
 # <<< conda initialize <<<
@@ -24,11 +24,9 @@ zoxide init fish --cmd cd | source
 # mise
 /opt/homebrew/opt/mise/bin/mise activate fish | source
 
-# Generated for envman. Do not edit.
-test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
-
 # force to venv to install packages
-set -g -x PIP_REQUIRE_VIRTUALENV true
+set -gx PIP_REQUIRE_VIRTUALENV true
+
 # set 1p sock
 set -eUgfl SSH_AUTH_SOCK
 set -Ux SSH_AUTH_SOCK ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
