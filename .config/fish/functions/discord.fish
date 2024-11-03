d86e5bbae62b0750e2196e885eab0464213d925c
kmg0695 <79101566+kmg0695@users.noreply.github.com>
2024-11-02 20:17:13 -0400

    Added discord fish alias

---
+++ .config/fish/functions/discord.fish
@@ -0,0 +1,4 @@
+function discord --wraps='open /Applications/Discord.app/Contents/MacOS/Discord' --description 'alias discord open /Applications/Discord.app/Contents/MacOS/Discord'
+  open /Applications/Discord.app/Contents/MacOS/Discord $argv
+        
+end
