3d60b06a18ef243894c70d6b973c3124e670d4b0
kmg0695 <79101566+kmg0695@users.noreply.github.com>
2025-08-11 16:49:11 -0400

    smol update

--- .config/fish/functions/bu.fish
+++ .config/fish/functions/bu.fish
@@ -1,4 +1,4 @@
-function bu --wraps='brew upgrade && fisher update && brew cleanup && brew autoremove && conda update --all --verbose --yes && mas upgrade' --description 'alias bu brew upgrade && fisher update && brew cleanup && brew autoremove && conda update --all --verbose --yes && mas upgrade'
-  brew upgrade && fisher update && brew cleanup --prune=all && brew autoremove && mas upgrade $argv
+function bu --wraps='brew upgrade && fisher update && brew cleanup --prune=all && brew autoremove && mas upgrade && uv self update && mise up' --description 'alias bu brew upgrade && fisher update && brew cleanup --prune=all && brew autoremove && mas upgrade && uv self update && mise up'
+  brew upgrade && fisher update && brew cleanup --prune=all && brew autoremove && mas upgrade && uv self update && mise up $argv
         
 end
