c71069d79578ad19100690eb79c602faed06d915
kmg0695 <79101566+kmg0695@users.noreply.github.com>
2025-09-15 15:29:00 -0400

    Added more macos defaults

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,10 +2,21 @@
 
 mac_settings() {
   # macOS Nice UI stuff
+  defaults write com.apple.dock autohide -bool true
   defaults write com.apple.dock autohide-delay -float 0
   defaults write com.apple.dock autohide-time-modifier -int 0
   defaults write com.apple.Dock showhidden -bool TRUE
   defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
+  defaults write com.apple.dock expose-animation-duration -float 0.1
+  defaults write com.apple.dock "mineffect" -string "scale"
+  defaults write com.apple.dock "tilesize" -int "48"
+
+  # Finder settings
+  defaults write com.apple.finder ShowPathbar -bool true
+  defaults write com.apple.finder ShowStatusBar -bool true
+  defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+  defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+  defaults write com.apple.finder AppleShowAllFiles -bool true
 
   # Aerospace specific commands
   defaults write com.apple.spaces spans-displays -bool true
@@ -14,10 +25,23 @@ mac_settings() {
   # move windows using ctrl + cmd
   defaults write -g NSWindowShouldDragOnGesture -bool
 
-  # restart said services
-  killall Dock
-  killall SystemUIServer
+  # Keyboard niceties
+  defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+  defaults write NSGlobalDomain KeyRepeat -int 1
+  defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
+  # MacOS Terminal stuff
+  defaults write com.apple.terminal StringEncodings -array 4
+  defaults write com.apple.terminal UnlimitedScrollback -bool true
+
+  # Disable Apple Intelligence crap
+  defaults write com.apple.CloudSubscriptionFeatures.optIn "545129924" -bool "false"
+
+  # AM Notif turn off (Very annoying)
+  defaults write com.apple.Music "userWantsPlaybackNotifications" -bool "false"
+
+  # restart said services
+  killall Dock SystemUIServer Finder Safari Music
 }
 
 # homebrew
