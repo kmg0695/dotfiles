29eef0b0ead9788dbcd2adc35f9826e7d60c0d00
kmg0695 <79101566+kmg0695@users.noreply.github.com>
2025-09-15 15:54:54 -0400

    Edited post.sh

--- scripts/post.sh
+++ scripts/post.sh
@@ -4,7 +4,7 @@
 brew bundle install --file ../brew/Brewfile
 
 # Make some config directories
-mkdir -p "$HOME"/.docker/ "$HOME"/.tmux/ "$HOME"/.config/fish/ "$HOME"/.config/kitty/ "$HOME"/Library/Application\ Support/Code/User
+mkdir -p "$HOME"/.docker/ "$HOME"/.tmux/ "$HOME"/.config/fish/ "$HOME"/Library/Application\ Support/Code/User
 
 # astronvim setup
 git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
@@ -21,12 +21,10 @@ git clone https://github.com/tmux-plugins/tpm "$HOME"/.tmux/plugins/tpm
 # shellcheck source=/dev/null
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 
-# fzf.fish
-fisher install PatrickF1/fzf.fish
-fzf_configure_bindings
-
-# autopair.fish
+# fisher plugins
+fisher install PatrickF1/fzf.fish && fzf_configure_bindings
 fisher install jorgebucaran/autopair.fish
+fisher install nickeb96/puffer-fish
 
 # Final
 #fish_add_path "$HOME"/go/bin
