f28a42c11d12dc8b4c2d00f29b8fbcdacbfe1c61
kmg0695 <79101566+kmg0695@users.noreply.github.com>
2025-09-16 01:20:05 -0400

    Added full path command substitutions

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 # Install brew stuff
-brew bundle install --file ../brew/Brewfile
+$(command -v brew) bundle install --file ../brew/Brewfile
 
 # Make some config directories
-mkdir -p "$HOME"/.docker/ "$HOME"/.tmux/ "$HOME"/.config/fish/ "$HOME"/Library/Application\ Support/Code/User
+$(command -v mkdir) -p "$HOME"/.docker/ "$HOME"/.tmux/ "$HOME"/.config/fish/ "$HOME"/.config/fastfetch "$HOME"/.config/mise
 
 # astronvim setup
-git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
-rm -rf ~/.config/nvim/.git
+$(command -v git) clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
+$(command -v rm) -rf ~/.config/nvim/.git
 
 # stow and git restore some items
-stow . --adopt
-git restore .
+$(command -v stow) . --adopt
+$(command -v git) restore .
 
 # tmux plugin manager
-git clone https://github.com/tmux-plugins/tpm "$HOME"/.tmux/plugins/tpm
+$(command -v git) clone https://github.com/tmux-plugins/tpm "$HOME"/.tmux/plugins/tpm
 
 # Fisher
 # shellcheck source=/dev/null
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+$(command -v curl) -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && $(command -v fisher) install jorgebucaran/fisher
 
 # fisher plugins
-fisher install PatrickF1/fzf.fish && fzf_configure_bindings
-fisher install jorgebucaran/autopair.fish
-fisher install nickeb96/puffer-fish
+$(command -v fisher) install PatrickF1/fzf.fish && fzf_configure_bindings
+$(command -v fisher) install jorgebucaran/autopair.fish
+$(command -v fisher) install nickeb96/puffer-fish
 
 # starship preset
-starship preset catppuccin-powerline -o ~/.config/starship.toml
+$(command -v starship) preset catppuccin-powerline -o ~/.config/starship.toml
 
 # Final
 #fish_add_path "$HOME"/go/bin
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,59 +2,59 @@
 
 mac_settings() {
   # macOS Nice UI stuff
-  defaults write com.apple.dock autohide -bool true
-  defaults write com.apple.dock autohide-delay -float 0
-  defaults write com.apple.dock autohide-time-modifier -int 0
-  defaults write com.apple.Dock showhidden -bool TRUE
-  defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
-  defaults write com.apple.dock expose-animation-duration -float 0.1
-  defaults write com.apple.dock "mineffect" -string "scale"
-  defaults write com.apple.dock "tilesize" -int "48"
+  $(command -v defaults) write com.apple.dock autohide -bool true
+  $(command -v defaults) write com.apple.dock autohide-delay -float 0
+  $(command -v defaults) write com.apple.dock autohide-time-modifier -int 0
+  $(command -v defaults) write com.apple.Dock showhidden -bool TRUE
+  $(command -v defaults) write -g NSAutomaticWindowAnimationsEnabled -bool false
+  $(command -v defaults) write com.apple.dock expose-animation-duration -float 0.1
+  $(command -v defaults) write com.apple.dock "mineffect" -string "scale"
+  $(command -v defaults) write com.apple.dock "tilesize" -int "48"
 
   # Finder settings
-  defaults write com.apple.finder ShowPathbar -bool true
-  defaults write com.apple.finder ShowStatusBar -bool true
-  defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-  defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-  defaults write com.apple.finder AppleShowAllFiles -bool true
+  $(command -v defaults) write com.apple.finder ShowPathbar -bool true
+  $(command -v defaults) write com.apple.finder ShowStatusBar -bool true
+  $(command -v defaults) write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+  $(command -v defaults) write NSGlobalDomain AppleShowAllExtensions -bool true
+  $(command -v defaults) write com.apple.finder AppleShowAllFiles -bool true
 
   # Aerospace specific commands
-  defaults write com.apple.spaces spans-displays -bool true
-  defaults write com.apple.dock expose-group-apps -bool true
+  $(command -v defaults) write com.apple.spaces spans-displays -bool true
+  $(command -v defaults) write com.apple.dock expose-group-apps -bool true
 
   # move windows using ctrl + cmd
-  defaults write -g NSWindowShouldDragOnGesture -bool
+  $(command -v defaults) write -g NSWindowShouldDragOnGesture -bool
 
   # Keyboard niceties
-  defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-  defaults write NSGlobalDomain KeyRepeat -int 1
-  defaults write NSGlobalDomain InitialKeyRepeat -int 15
+  $(command -v defaults) write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+  $(command -v defaults) write NSGlobalDomain KeyRepeat -int 1
+  $(command -v defaults) write NSGlobalDomain InitialKeyRepeat -int 15
 
   # MacOS Terminal stuff
-  defaults write com.apple.terminal StringEncodings -array 4
-  defaults write com.apple.terminal UnlimitedScrollback -bool true
+  $(command -v defaults) write com.apple.terminal StringEncodings -array 4
+  $(command -v defaults) write com.apple.terminal UnlimitedScrollback -bool true
 
   # Disable Apple Intelligence crap
-  defaults write com.apple.CloudSubscriptionFeatures.optIn "545129924" -bool "false"
+  $(command -v defaults) write com.apple.CloudSubscriptionFeatures.optIn "545129924" -bool "false"
 
   # AM Notif turn off (Very annoying)
-  defaults write com.apple.Music "userWantsPlaybackNotifications" -bool "false"
+  $(command -v defaults) write com.apple.Music "userWantsPlaybackNotifications" -bool "false"
 
   # restart said services
-  killall Dock SystemUIServer Finder Safari Music
+  $(command -v killall) Dock SystemUIServer Finder Safari Music
 }
 
 # homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval "$(/opt/homebrew/bin/brew shellenv)"
+$(command -v eval) "$(/opt/homebrew/bin/brew shellenv)"
 
 # set fish as default shell
-echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells
-chsh -s /opt/homebrew/bin/fish
+$(command -v echo) /opt/homebrew/bin/fish | sudo tee -a /etc/shells
+$(command -v chsh) -s /opt/homebrew/bin/fish
 
 mac_settings
 
 # Install post prerequisites
-brew install stow fish
+$(command -v brew) install stow fish
 
 exit
